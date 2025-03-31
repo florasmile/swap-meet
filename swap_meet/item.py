09f2635f26219e5eff7c9eb3fda384e418c552a5
dehuihu <dehuihu@gmail.com>
2025-03-31 16:15:14 -0700

    Implemented Item class in wave_2 and passed tests

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,2 +1,9 @@
+import uuid
 class Item:
-    pass
+    def __init__(self, id = None):
+        self.id = uuid.uuid4().int if id is None else id
+    
+    def get_category(self):
+        return self.__class__.__name__
+    
+    
