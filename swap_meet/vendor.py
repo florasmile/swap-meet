193d685c5582fe575f0874bacef36f2cc6f5739a
dehuihu <dehuihu@gmail.com>
2025-03-31 16:22:26 -0700

    Implemented get_by_id in vendor in wave_2

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -12,3 +12,9 @@ class Vendor:
         
         self.inventory.remove(item)
         return item
+    
+    def get_by_id(self, id):
+        for item in self.inventory:
+            if item.id == id:
+                return item
+        return None
