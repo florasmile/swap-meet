8ee2e8cce12e6af48b7520316c8d7fb522085180
Lina Martinez <lmcastro3501@gmail.com>
2025-04-01 15:02:14 -0700

    implements constructor and __str__ methods. Clothing.py in Wave 5

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -1,2 +1,11 @@
-class Clothing:
-    pass
+from swap_meet.item import Item
+
+class Clothing(Item):
+    def __init__(self, id = None, fabric = "Unknown"):
+        super().__init__(id)
+        self.fabric = fabric
+    
+    def __str__(self):
+        general_description = super().__str__()
+        return f"{general_description} It is made from {self.fabric} fabric."
+    
