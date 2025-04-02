ce48db57800c51e7e45ec9dff906ca5782c08b22
Lina Martinez <lmcastro3501@gmail.com>
2025-04-02 13:19:56 -0700

    implements electronics class. Wave 5

--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -1,2 +1,13 @@
-class Electronics:
-    pass
+from swap_meet.item import Item
+
+class Electronics(Item):
+    def __init__(self, id=None, type="Unknown"):
+        super().__init__(id)
+        self.type = type
+    
+    def __str__(self):
+        general_description = super().__str__()
+        return " ".join([general_description, self.display_type_message()])
+    
+    def display_type_message(self):
+        return f"This is a {self.type} device."
